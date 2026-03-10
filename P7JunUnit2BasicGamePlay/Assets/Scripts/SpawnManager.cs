15c228f3972fe67cf87a343c7c0cb3c024fef454
DozingDozerZZZZ <763664@student.dusd.net>
2026-03-10 15:27:04 -0700

    Detect collisions script not working

--- P7JunUnit2BasicGamePlay/Assets/Scripts/SpawnManager.cs
+++ P7JunUnit2BasicGamePlay/Assets/Scripts/SpawnManager.cs
@@ -3,16 +3,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating(SpawnRandomAnimal,startDelay,spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-10, 10), 0, 20);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
